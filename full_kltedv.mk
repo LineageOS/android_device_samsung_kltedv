5b9d188acabeecc87d16c9cfe9215b66bf85ddc3
Matt Filetto <matt.filetto@gmail.com>
2014-10-21 23:25:05 -0700

    kltedv: change the remaining references from "klte" to "kltedv"
    * I somehow missed these during bringup. My bad :(
    
    Change-Id: I191be768cf92b804a0b8f3ad3f9840fe95f90366

--- full_kltedv.mk
+++ full_kltedv.mk
@@ -16,11 +16,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from klte device
-$(call inherit-product, device/samsung/klte/device.mk)
+$(call inherit-product, device/samsung/kltedv/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_kltexx
-PRODUCT_DEVICE := kltexx
+PRODUCT_NAME := full_kltedv
+PRODUCT_DEVICE := kltedv
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := kltedv
