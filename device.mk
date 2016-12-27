5d677ca0d5b28783c5ccd8fb8973f1cfe50b072f
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-27 10:31:42 -0700

    kltedv: Import gps.conf file into device tree
    * This isn't any more proprietary than the audio configuration and
      should be able to be edited as such
    
    Change-Id: Id27df559000995e35a7d891c4317f67e65bdd3aa

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +22,10 @@ $(call inherit-product-if-exists, vendor/samsung/kltedv/kltedv-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
 
 # NFC
 PRODUCT_PACKAGES += \
