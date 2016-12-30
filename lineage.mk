0a9356b30cacf346f0f31084a4cfe35c7e942f56
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-29 22:03:01 -0700

    kltedv: Rebrand some of the things
    * Update PRODUCT_NAME because the value is user-visable
    * Rename cm.mk to lineage.mk because we are touching the file
    * Rename cm.dependencies to lineage.dependencies because why not
    
    Change-Id: I5c042e03b30e319bf20209a6a37278e85c9493be

--- lineage.mk
+++ lineage.mk
@@ -7,4 +7,4 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := kltedv
-PRODUCT_NAME := cm_kltedv
+PRODUCT_NAME := lineage_kltedv
