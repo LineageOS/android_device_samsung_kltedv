dafe7f90e335d7187771ca10ed3cda16d4fdfa54
Paul Keith <javelinanddart@gmail.com>
2017-10-19 23:19:53 +0200

    kltedv: Remove useless prop for kltespr
    * This is unused
    
    Change-Id: I3ba8605f8baeb1a2cf9bfd96e3e635ac2c9cb396

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -45,7 +45,6 @@ void cdma_properties(char const *default_cdma_sub,
     property_set("ro.cdma.home.operator.alpha", operator_alpha);
     property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
     property_set("ro.telephony.default_network", "10");
-    property_set("ro.telephony.ril.config", "newDriverCallU,newDialCode");
     property_set("telephony.lteOnCdmaDevice", "1");
 }
 
